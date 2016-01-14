ab1ba6f95a7251960665813f421903a138f07e76
Lukasz_Gosiewski <lukigostek@gmail.com>
2016-01-14 17:04:58 +0100

    Icons added

--- app/src/main/java/com/gosiewski/shoppinglist/activities/ListDetailsActivity.java
+++ app/src/main/java/com/gosiewski/shoppinglist/activities/ListDetailsActivity.java
@@ -1,10 +1,10 @@
 package com.gosiewski.shoppinglist.activities;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
